f55ca22869058bf5b0d33d525c875c0ed89788ec
Jacobo <jacobo.a12@utp.edu.co>
2020-11-20 19:03:44 -0500

    Se agrega el llamado a función

--- bubble_sort.py
+++ bubble_sort.py
@@ -16,6 +16,7 @@ def bubbleSort(arr):
   
 # Driver code to test above 
 arr = [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr)
 print ("Sorted array is:") 
 for i in range(len(arr)): 
     print ("%d" %arr[i]), 
