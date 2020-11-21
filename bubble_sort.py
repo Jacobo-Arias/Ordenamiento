d268116223af1676a9f185a99fade0d0fef198e3
Jacobo Arias <jacobo.a12@utp.edu.co>
2020-11-21 16:09:33 -0500

    Se optimiza burbuja con una bandera swap

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,21 +1,26 @@
 # Python program for implementation of Bubble Sort 
+from time import time
 def bubbleSort(arr): 
     n = len(arr) 
-  
     # Traverse through all array elements 
     for i in range(n): 
-  
+
         # Last i elements are already in place 
+        swap = False
         for j in range(0, n-i-1): 
-  
             # traverse the array from 0 to n-i-1 
             # Swap if the element found is greater 
             # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
-  
+                swap = True
+        if not swap:
+            break
+
+
 # Driver code to test above 
 arr = [64, 34, 25, 12, 22, 11, 90]
+# arr = [1,2,3,4,5,6,7,8,9,10,11,13,12]
 bubbleSort(arr)
 print ("Sorted array is:") 
 for i in range(len(arr)): 
--- metods.py
+++ metods.py
@@ -1,54 +1,56 @@
 # Python program for implementation of Bubble Sort 
 def bubbleSort(arr): 
     n = len(arr) 
-  
     # Traverse through all array elements 
     for i in range(n): 
-  
+
         # Last i elements are already in place 
+        swap = False
         for j in range(0, n-i-1): 
-  
             # traverse the array from 0 to n-i-1 
             # Swap if the element found is greater 
             # than the next element 
             if arr[j] > arr[j+1] : 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
-  
+                swap = True
+        if not swap:
+            break 
 
 
- 
+
+
 def heapify(arr, n, i):
     largest = i  # Initialize largest as root
     l = 2 * i + 1     # left = 2*i + 1
     r = 2 * i + 2     # right = 2*i + 2
- 
+
     # See if left child of root exists and is
     # greater than root
     if l < n and arr[largest] < arr[l]:
         largest = l
- 
+
     # See if right child of root exists and is
     # greater than root
     if r < n and arr[largest] < arr[r]:
         largest = r
- 
+
     # Change root, if needed
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]  # swap
- 
+
         # Heapify the root.
         heapify(arr, n, largest)
- 
+
 # The main function to sort an array of given size
- 
- 
+
+
 def heapSort(arr):
     n = len(arr)
- 
+
     # Build a maxheap.
     for i in range(n//2 - 1, -1, -1):
         heapify(arr, n, i)
- 
+
     # One by one extract elements
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]  # swap
